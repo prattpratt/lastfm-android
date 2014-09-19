39c34b09b7d71791de2e25d6508392147c2cde56
Siarhei Valiulik <siarhei_valiulik@epam.com>
2014-09-19 17:37:20 +0300

    Update build config

---
+++ app/src/fm/last/android/PrivateAPIKey.java
@@ -0,0 +1,8 @@
+package fm.last.android;
+
+public class PrivateAPIKey {
+
+	public static final String KEY = "f77a99a8cc94de28f48cb9dfbff8723a";
+	public static final String SECRET = "dbadd361823c419d9975de34baff3e86";
+	public static final String ANALYTICS_ID = "UA-11993480-1"; //leave this empty, or provide your own Google Analytics ID
+}
